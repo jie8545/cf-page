d6713d37ce2809e5940f070a1699597bf2b04104
jie8545 <65769506+jie8545@users.noreply.github.com>
2022-05-29 13:43:10 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="hero.inpai.ga";
+        url.hostname="her.inpai.ga";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
